deff63dc006270cfe03012c08ffd41c030f7d9a4
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2020-08-08 00:26:27 +0530

    portfolio_v1

---
+++ PortFolio/script1.js
@@ -0,0 +1,5 @@
+var btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  location.href =
+    "https://drive.google.com/file/d/12ccBJssAx6b7AAARUlLKYKJLyqSbPSFG/view?usp=sharing";
+});
