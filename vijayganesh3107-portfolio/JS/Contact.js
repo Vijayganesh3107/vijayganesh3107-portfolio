0d7de5b53f0b2ef72a10ad82962cd80aca02bb14
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2020-09-02 21:50:35 +0530

    Commiting All

---
+++ vijayganesh3107-portfolio/JS/Contact.js
@@ -0,0 +1,17 @@
+let twitter = document.querySelector(".twitter");
+let facebook = document.querySelector(".facebook");
+let github = document.querySelector(".github");
+let instagram = document.querySelector(".instagram");
+
+twitter.addEventListener("click", () => {
+  location.href = "https://twitter.com/vijayga90091961";
+});
+facebook.addEventListener("click", () => {
+  location.href = "https://www.facebook.com/vijay.ganesh.7505";
+});
+instagram.addEventListener("click", () => {
+  location.href = "https://www.instagram.com/vijayganesh_vg/?hl=en";
+});
+github.addEventListener("click", () => {
+  location.href = "https://github.com/Vijayganesh3107";
+});
---
+++ vijayganesh3107-portfolio/JS/Projects.js
@@ -0,0 +1,78 @@
+var netlifyurlclock = document.getElementById("netlifyurlclock");
+var githubclock = document.getElementById("githubclock");
+var netlifyurllanding = document.getElementById("netlifyurllanding");
+var githublanding = document.getElementById("githublanding");
+var netlifyurlcalculator = document.getElementById("netlifyurlcalculator");
+var githubcalculator = document.getElementById("githubcalculator");
+var netlifyurltower = document.getElementById("netlifyurltower");
+var githubtower = document.getElementById("githubtower");
+var netlifyurlstories = document.getElementById("netlifyurlstories");
+var githubstories = document.getElementById("githubstories");
+var netlifyurlimdb = document.getElementById("netlifyurlimdb");
+var githubimdb = document.getElementById("githubimdb");
+var netlifyurlwheather = document.getElementById("netlifyurlwheather");
+var githubwheather = document.getElementById("githubwheather");
+var netlifyurltictactoe = document.getElementById("netlifyurltictactoe");
+var githubtictactoe = document.getElementById("githubtictactoe");
+
+netlifyurlclock.addEventListener("click", () => {
+  location.href = "https://dazzling-babbage-dbc251.netlify.app";
+});
+githubclock.addEventListener("click", () => {
+  location.href =
+    "https://github.com/Vijayganesh3107/Vijayganesh3107.github.io/tree/master/Analog%20CLock";
+});
+
+netlifyurllanding.addEventListener("click", () => {
+  location.href = "https://peaceful-borg-ea78da.netlify.app/";
+});
+githublanding.addEventListener("click", () => {
+  location.href =
+    "https://github.com/Vijayganesh3107/Vijayganesh3107.github.io/tree/master/Landing%20Page";
+});
+
+netlifyurlcalculator.addEventListener("click", () => {
+  location.href = "https://determined-poitras-b93d4e.netlify.app/";
+});
+githubcalculator.addEventListener("click", () => {
+  location.href =
+    "https://github.com/Vijayganesh3107/Vijayganesh3107.github.io/tree/master/Calculator_original";
+});
+
+netlifyurltower.addEventListener("click", () => {
+  location.href = "https://keen-roentgen-3c040a.netlify.app/";
+});
+githubtower.addEventListener("click", () => {
+  location.href = "https://github.com/Vijayganesh3107/Hackathorn";
+});
+
+netlifyurlstories.addEventListener("click", () => {
+  location.href = "https://newyork-times-api-vijay.netlify.app";
+});
+githubstories.addEventListener("click", () => {
+  location.href = "https://github.com/Vijayganesh3107/newyork-time-api";
+});
+
+netlifyurlimdb.addEventListener("click", () => {
+  location.href = "https://pensive-mirzakhani-9c5229.netlify.app/index3.html";
+});
+githubimdb.addEventListener("click", () => {
+  location.href =
+    "https://github.com/Vijayganesh3107/Vijayganesh3107.github.io/tree/master/IMDB%20Rating";
+});
+
+netlifyurlwheather.addEventListener("click", () => {
+  location.href = "https://pensive-mirzakhani-9c5229.netlify.app/index1.html";
+});
+githubwheather.addEventListener("click", () => {
+  location.href =
+    "https://github.com/Vijayganesh3107/Vijayganesh3107.github.io/tree/master/Wheather%20Async%20Await";
+});
+
+netlifyurltictactoe.addEventListener("click", () => {
+  location.href = "https://vijay-tic-tac-toe.netlify.app";
+});
+githubtictactoe.addEventListener("click", () => {
+  location.href =
+    "https://github.com/Vijayganesh3107/Vijayganesh3107.github.io/tree/master/TicTacToe";
+});
